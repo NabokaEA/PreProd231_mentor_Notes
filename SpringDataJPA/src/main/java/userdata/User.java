687e42ef0f30a2545f484f93e07d14ccef51fc45
NabokaEA <nabokae@yandex.ru>
2022-09-11 22:03:48 +0300

    Создание entity User

---
+++ SpringDataJPA/src/main/java/userdata/User.java
@@ -0,0 +1,40 @@
+package userdata;
+
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String passWord;
+
+
+    public User(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+}
